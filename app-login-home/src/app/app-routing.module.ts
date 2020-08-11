7fe3e19b64bc23589891147d29c81db51b990a31
LemHammaden <c.gardner941@gmail.com>
2020-08-11 17:35:09 +0000

    [sidebar and navbar, first try]

--- app-login-home/src/app/app-routing.module.ts
+++ app-login-home/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './login-form.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
-  { path: 'home', component: AppComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
--- app-login-home/src/app/app.module.ts
+++ app-login-home/src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent, LoginFormComponent, HomeComponent, NavbarComponent, SidebarComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
