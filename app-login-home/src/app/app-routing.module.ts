043296509f3cbcdcb7c20e922b8418f304203a03
SIDI <a.sidielmoctar@gmail.com>
2020-08-11 11:17:46 +0000

    refactoring: pour routing, c'est mieux d'utiliser le module propre au routage approuting module dans notre cas

--- app-login-home/src/app/app-routing.module.ts
+++ app-login-home/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login-form.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+  { path: 'home', component: AppComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- app-login-home/src/app/app.module.ts
+++ app-login-home/src/app/app.module.ts
@@ -5,20 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { routing } from './app.routing'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    routing
-  ],
+  declarations: [AppComponent, LoginFormComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
